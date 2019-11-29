8ad052fd39607ce8e9551a4d3bc4be15843ccb4f
karmiy <532981481@qq.com>
2019-11-29 17:07:36 +0800

    utils/date.js 错误变量

--- JavaScript/utils/date.js
+++ JavaScript/utils/date.js
@@ -17,7 +17,7 @@ export function formatDate(date, fmt) {
     };
     if (/(Y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (let k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
 
