dad4ae2871474aaba1e486f7ec67391ddaf2aa35
Administrator <532981481@qq.com>
2019-12-01 19:44:53 +0800

    formatDate 格式化修复位数问题

--- JavaScript/utils/date.js
+++ JavaScript/utils/date.js
@@ -1,3 +1,4 @@
+import { leftPad } from './base';
 /**
  * 格式化日期
  * @param date: Date
@@ -15,7 +16,7 @@ export function formatDate(date, fmt) {
         "q+": Math.floor((date.getMonth() + 3) / 3), // 季度
         "S": date.getMilliseconds() //毫秒
     };
-    if (/(Y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    if (/(Y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, leftPad((date.getFullYear() + "").substr(4 - RegExp.$1.length), RegExp.$1.length, '0'));
     for (let k in o)
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
