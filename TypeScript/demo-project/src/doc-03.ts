9eff2c7f612b681a7f8dd05b4b93d280d90d639c
Administrator <532981481@qq.com>
2020-01-09 11:02:18 +0800

    pending: 其他类型

--- TypeScript/demo-project/src/doc-03.ts
+++ TypeScript/demo-project/src/doc-03.ts
@@ -1,3 +1,5 @@
+// 原始类型
+
 // 1、boolean
 const isLoading: boolean = false;
 
@@ -26,4 +28,60 @@ const u: undefined = undefined;
 // 但是项目开发中一般开启 --strictNullChecks
 // 即 null 与 undefined 只能赋值给 any 与它们自己(特例 undefined 可以给 void)
 // 即 const nu: number = null; 默认是可以的，strictNullChecks 下不行
+
+// 6、symbol
+// 使用 symbol，需要在 tsconfig.json 里添加 es6 编译辅助库:
+// "lib": ["es6", "dom"]
+
+const sym: symbol = Symbol('key');
+
+// 7、BigInt
+// 在 TypeScript3.2 版本内置，可以安全地存储和操作大整数
+// 使用 BigInt，需要在 tsconfig.json 里添加 ESNext 编译辅助库:
+// "lib": ["es6", "dom", "ESNext"]
+const bigNumber: bigint = BigInt(Number.MAX_SAFE_INTEGER);
+
+// 其他类型
+
+// any
+let a: any = 4;
+a = '123'; // ok
+
+// unknown
+let any_value: any;
+
+any_value = true;             // OK
+any_value = 1;                // OK
+any_value = "Hello World";    // OK
+any_value = Symbol("type");   // OK
+any_value = {}                // OK
+any_value = []                // OK
+
+let unknown_value: unknown;
+
+unknown_value = true;             // OK
+unknown_value = 1;                // OK
+unknown_value = "Hello World";    // OK
+unknown_value = Symbol("type");   // OK
+unknown_value = {}                // OK
+unknown_value = []                // OK
+
+// 区别
+any_value.foo.bar;  // OK
+any_value();        // OK
+new any_value();    // OK
+any_value[0][1];    // OK
+
+// unknown_value.foo.bar;  // ERROR
+// unknown_value();        // ERROR
+// new unknown_value();    // ERROR
+// unknown_value[0][1];    // ERROR
+
+function getStrValue(value: unknown): string {
+    if(value instanceof Date) {
+        return value.toISOString();
+    }
+    return String(value);
+}
+
 
