d67398d6ddadb99f6095e60a8b0d15041ce3cde5
Administrator <532981481@qq.com>
2020-04-25 15:30:53 +0800

    设计模式：代理模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,16 +1,25 @@
+const createProxyFactory = function (fn) {
+    const cache = {};
+    return function (...args) {
+        const key = args.join(',');
+        if(key in cache) return cache[key];
+
+        return cache[key] = fn.apply(this, args);
+    }
+};
+
 const mult = function(...args) {
     return args.reduce((sum, item) => sum * item);
 }
 
-const proxyMult = (function() {
-    const cache = {};
+const add = function(...args) {
+    return args.reduce((sum, item) => sum + item);
+}
 
-    return function(...args) {
-        const key = args.join(',');
-        if(key in cache) return cache[key];
+const proxyMult = createProxyFactory(mult);
+const proxyAdd = createProxyFactory(add);
 
-        return cache[key] = mult.apply(this, args);
-    }
-}());
-
-proxyMult(1, 2, 3);
+console.log(proxyMult(1, 2, 3));
+console.log(proxyMult(1, 2, 3));
+console.log(proxyAdd(2, 3));
+console.log(proxyAdd(2, 3));
