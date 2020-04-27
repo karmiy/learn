0a53fbe797b75ae900b5d88d9d3a807e3957440c
karmiy <532981481@qq.com>
2020-04-27 23:38:15 +0800

    设计模式：迭代器模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,25 +1,25 @@
-const createProxyFactory = function (fn) {
-    const cache = {};
-    return function (...args) {
-        const key = args.join(',');
-        if(key in cache) return cache[key];
-
-        return cache[key] = fn.apply(this, args);
+const Iterator = function(obj) {
+    let current = 0;
+    const len = obj.length;
+    const next = () => current += 1;
+    const isDone = () => current >= len;
+    const getCurrItem = () => obj[current];
+    return {
+        next,
+        isDone,
+        getCurrItem,
     }
 };
 
-const mult = function(...args) {
-    return args.reduce((sum, item) => sum * item);
+const compare = function(iterator1, iterator2) {
+    while (!iterator1.isDone() && !iterator2.isDone()) {
+        if (iterator1.getCurrItem() !== iterator2.getCurrItem()) {
+            throw new Error('iterator1 和 iterator2 不相等');
+        }
+        iterator1.next();
+        iterator2.next();
+    }
+    console.log('相等');
 }
 
-const add = function(...args) {
-    return args.reduce((sum, item) => sum + item);
-}
-
-const proxyMult = createProxyFactory(mult);
-const proxyAdd = createProxyFactory(add);
-
-console.log(proxyMult(1, 2, 3));
-console.log(proxyMult(1, 2, 3));
-console.log(proxyAdd(2, 3));
-console.log(proxyAdd(2, 3));
+compare(Iterator([1, 2]), Iterator([1, 2]));
