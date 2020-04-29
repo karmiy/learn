f3abee006247336e0f65bce39031a977febf06fe
karmiy <532981481@qq.com>
2020-04-30 00:51:56 +0800

    设计模式：迭代器模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,10 +1,31 @@
-const each = function(ary, callback) {
-    for (let i = 0, l = ary.length; i < l; i++) {
-        if(callback(i, ary[i]) === false) break;
+const getActiveUploadObj = function() {
+    try {
+        return new ActiveXObject("TXFTNActiveX.FTNUpload"); // IE 上传控件
+    } catch (e) {
+        return false;
     }
 };
 
-each([1, 2, 3], function(i, n) {
-    if(i > 1) return false;
-    console.log(i, n);
-});
+const getFlashUploadObj = function() {
+    if (supportFlash()) { // supportFlash 函数未提供
+        const str = '<object type="application/x-shockwave-flash"></object>';
+        return $(str).appendTo($('body'));
+    }
+    return false;
+};
+
+const getFormUpladObj = function() {
+    const str = '<input name="file" type="file" class="ui-file"/>'; // 表单上传
+    return $(str).appendTo($('body'));
+};
+
+const iteratorUploadObj = function(...args) {
+    for (let i = 0, fn; fn = args[i++];) {
+        const uploadObj = fn();
+        if (uploadObj !== false) {
+            return uploadObj;
+        }
+    }
+};
+
+iteratorUploadObj(getActiveUploadObj, getFlashUploadObj, getFormUpladObj);
