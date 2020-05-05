2f44cb39fa74bbca922032c5a262f8eb7fb5adaf
karmiy <532981481@qq.com>
2020-05-05 22:36:17 +0800

    设计模式：观察者模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,38 +1,47 @@
-const Event = (function() {
-    const subscribers = {};
+const event = {
+    observers: [],
+    addObserver(key, fn) {
+        if (!this.observers[key]) this.observers[key] = [];
 
-    const subscribe = function(key, fn) {
-        if (!subscribers[key]) subscribers[key] = [];
-
-        subscribers[key].push(fn);
-    };
-    const publish = function(key, ...args) {
-        const fns = subscribers[key];
+        this.observers[key].push(fn); // 订阅的消息添加进缓存列表
+    },
+    notify(key, ...args) {
+        const fns = this.observers[key];
         if (!fns || fns.length === 0) return false;
 
         fns.forEach(fn => fn(...args));
-    };
-    const unsubscribe = function(key, fn) {
-        const fns = subscribers[key];
-        if (!fns || fns.length === 0) return false;
-
-        if (!fn) {
-            fns && (fns.length = 0);
+    },
+    removeObserver(key, fn) {
+        const fns = this.observers[key];
+        if(!fns || fns.length === 0) return false;
+        
+        if(!fn) {
+            fns && (fns.length = 0); // 如果没有传具体回调，取消 key 对应的所有订阅
         } else {
-            for (let l = fns.length - 1; l >= 0; l--) {
+            for(let l = fns.length - 1; l >=0; l--) {
                 const _fn = fns[l];
-                if (_fn === fn) fns.splice(l, 1);
+                if(_fn === fn) fns.splice(l, 1);
             }
         }
-    };
-    return {
-        subscribe,
-        publish,
-        unsubscribe,
     }
-})();
+};
 
-Event.subscribe('squareMeter88', function(price) { // A 订阅消息
-    console.log('价格= ' + price);
-});
-Event.publish('squareMeter88', 2000000);
+// 定义安装函数，让对象动态安装观察者功能
+const installEvent = function(obj) {
+    for (let key in event) {
+        obj[key] = event[key];
+    }
+};
+
+
+const salesOffices = {};
+installEvent(salesOffices);
+
+const fn_a = function(price) {
+    console.log('A: 价格= ' + price);
+}
+salesOffices.addObserver('squareMeter88', fn_a);
+salesOffices.notify('squareMeter88', 2000000); // 输出: A: 价格= 2000000
+
+salesOffices.removeObserver('squareMeter88', fn_a);
+salesOffices.notify('squareMeter88', 2000000); // 取消了订阅，无输出
