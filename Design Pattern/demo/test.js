d7a68f09c09feda1ae9be413e1f637ff00ccbec7
karmiy <532981481@qq.com>
2020-05-14 17:59:44 +0800

    设计模式：职责链模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,26 +1,29 @@
-const poolFactory = function(createFn) {
-    const pool = [];
-
-    return {
-        create(...args) {
-            const obj = pool.length ? pool.shift() : createFn.apply(this, ...args);
-        },
-        recover(obj) {
-            pool.push(obj);
-        }
+const order500 = function(orderType, pay, stock) {
+    if (orderType === 1 && pay) {
+        console.log('500 元定金预购, 得到 100 优惠券');
+    } else {
+        order200(orderType, pay, stock); // 将请求传递给 200 元订单
     }
 }
 
-const tooltipFactory = poolFactory(function() {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    return div;
-});
-
-const arr = [];
-
-for (let i = 0, str; str = ['1', '2'][i++];) {
-    const tooltip = tooltipFactory.create();
-    tooltip.innerHTML = str;
-    arr.push(tooltip);
+const order200 = function(orderType, pay, stock) {
+    if (orderType === 2 && pay) {
+        console.log('200 元定金预购, 得到 50 优惠券');
+    } else {
+        orderNormal(orderType, pay, stock); // 将请求传递给普通订单
+    }
 }
+
+const orderNormal = function(orderType, pay, stock) {
+    if (stock > 0) {
+        console.log('普通购买, 无优惠券');
+    } else {
+        console.log('手机库存不足');
+    }
+}
+
+order500(1, true, 500); // 输出：500 元定金预购, 得到 100 优惠券
+order500(1, false, 500); // 输出：普通购买, 无优惠券
+order500(2, true, 500); // 输出：200 元定金预购, 得到 500 优惠券
+order500(3, false, 500); // 输出：普通购买, 无优惠券
+order500(3, false, 0); // 输出：手机库存不足
