cc202267ffd1b77503fbe77157c7506c4682f0de
karmiy <532981481@qq.com>
2020-05-17 23:23:18 +0800

    设计模式：中介者模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,24 +1,3 @@
-const getActiveUploadObj = function() {
-    try {
-        return new ActiveXObject("TXFTNActiveX.FTNUpload"); // IE 上传控件
-    } catch (e) {
-        return false;
-    }
-};
-
-const getFlashUploadObj = function() {
-    if (supportFlash()) { // supportFlash 函数未提供
-        const str = '<object type="application/x-shockwave-flash"></object>';
-        return $(str).appendTo($('body'));
-    }
-    return false;
-};
-
-const getFormUpladObj = function() {
-    const str = '<input name="file" type="file" class="ui-file"/>'; // 表单上传
-    return $(str).appendTo($('body'));
-};
-
-const getUploadObj = getActiveUploadObj.after(getFlashUploadObj).after(getFormUpladObj);
-
-getUploadObj();
+class Player {
+    
+}
