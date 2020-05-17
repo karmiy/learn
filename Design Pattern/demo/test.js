dfb2898e7e9841b141bb90f7ad279790f4636714
karmiy <532981481@qq.com>
2020-05-17 21:20:17 +0800

    设计模式：职责链模式

--- Design Pattern/demo/test.js
+++ Design Pattern/demo/test.js
@@ -1,29 +1,24 @@
-const order500 = function(orderType, pay, stock) {
-    if (orderType === 1 && pay) {
-        console.log('500 元定金预购, 得到 100 优惠券');
-    } else {
-        order200(orderType, pay, stock); // 将请求传递给 200 元订单
+const getActiveUploadObj = function() {
+    try {
+        return new ActiveXObject("TXFTNActiveX.FTNUpload"); // IE 上传控件
+    } catch (e) {
+        return false;
     }
-}
+};
 
-const order200 = function(orderType, pay, stock) {
-    if (orderType === 2 && pay) {
-        console.log('200 元定金预购, 得到 50 优惠券');
-    } else {
-        orderNormal(orderType, pay, stock); // 将请求传递给普通订单
+const getFlashUploadObj = function() {
+    if (supportFlash()) { // supportFlash 函数未提供
+        const str = '<object type="application/x-shockwave-flash"></object>';
+        return $(str).appendTo($('body'));
     }
-}
+    return false;
+};
 
-const orderNormal = function(orderType, pay, stock) {
-    if (stock > 0) {
-        console.log('普通购买, 无优惠券');
-    } else {
-        console.log('手机库存不足');
-    }
-}
+const getFormUpladObj = function() {
+    const str = '<input name="file" type="file" class="ui-file"/>'; // 表单上传
+    return $(str).appendTo($('body'));
+};
 
-order500(1, true, 500); // 输出：500 元定金预购, 得到 100 优惠券
-order500(1, false, 500); // 输出：普通购买, 无优惠券
-order500(2, true, 500); // 输出：200 元定金预购, 得到 500 优惠券
-order500(3, false, 500); // 输出：普通购买, 无优惠券
-order500(3, false, 0); // 输出：手机库存不足
+const getUploadObj = getActiveUploadObj.after(getFlashUploadObj).after(getFormUpladObj);
+
+getUploadObj();
